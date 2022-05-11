429fbdac4f985966aaa2fd2d15bc96fc89ca4a7c
srbernardo <73710552+srbernardo@users.noreply.github.com>
2022-05-11 16:18:08 -0300

    Delete iFace/projeto directory

--- iFace/projeto/Interfaces/Check_Comunidade/Check_Comunidade.java
+++
@@ -1,9 +0,0 @@
-package Interfaces.Check_Comunidade;
-
-import java.util.ArrayList;
-
-import Comunidades.Comunidade.Comunidade;
-
-public interface Check_Comunidade {
-  int check_c(String nome, ArrayList<Comunidade> comunidades);
-}
--- iFace/projeto/Interfaces/Check_Usuario/Check_Usuario.java
+++
@@ -1,9 +0,0 @@
-package Interfaces.Check_Usuario;
-
-import java.util.ArrayList;
-
-import Usuario.Usuario;
-
-public interface Check_Usuario {
-  int check_u(String login, ArrayList<Usuario> usuarios);
-}
