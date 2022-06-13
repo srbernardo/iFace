a81dc3a5c81bde3d07e90e6ae82e4d82794bda76
srbernardo <73710552+srbernardo@users.noreply.github.com>
2022-06-13 16:18:03 -0300

    Delete projeto directory

--- projeto/Exception/ErroDeDado.java
+++
@@ -1,9 +0,0 @@
-package Exception;
-import java.util.Scanner;
-
-public class ErroDeDado extends RuntimeException {
-  @Override
-  public String getMessage() {
-    return "Erro de tipo na digitação do campo.";
-  }  
-}
--- projeto/Interfaces/Checar_Existencia/Checar_Existencia.java
+++
@@ -1,7 +0,0 @@
-package Interfaces.Checar_Existencia;
-
-import java.util.ArrayList;
-
-public interface Checar_Existencia {
-  <E> int checar_existencia(String param1, ArrayList<E> array);
-}
